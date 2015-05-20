a724dddb5f805fea256818a93278e7941fa30aab
huanz <260540233@qq.com>
2015-05-20 18:54:46 +0800

    code format

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var RevAll = require('gulp-rev-all');
 
 
 gulp.task('default', function() {
-	var revAll = new RevAll({
-		dontGlobal: ['.json'],
-		dontRenameFile: [/^\/views\/(.*)/i, /^\/js\/libs\/(.*)\.js/i, /^\/js\/sea-modules\/(.*).js/i],
-		dontUpdateReference: [ /^\/views\/(.*)/i, /^\/js\/libs\/(.*)\.js/i, /^\/js\/sea-modules\/(.*).js/i]
-	});
+    var revAll = new RevAll({
+        dontGlobal: ['.json'],
+        dontRenameFile: [/^\/views\/(.*)/i, /^\/js\/libs\/(.*)\.js/i, /^\/js\/sea-modules\/(.*).js/i],
+        dontUpdateReference: [/^\/views\/(.*)/i, /^\/js\/libs\/(.*)\.js/i, /^\/js\/sea-modules\/(.*).js/i]
+    });
     gulp.src('src/**')
         .pipe(revAll.revision())
         .pipe(gulp.dest('dist'))
         .pipe(revAll.manifestFile())
-        .pipe(gulp.dest('./')); 
+        .pipe(gulp.dest('./'));
 });
